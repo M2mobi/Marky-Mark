99b10b32e6d79e43fd423eeb2ee9aa5d22a56ca0
Jim van Zummeren <jim@m2mobi.com>
2017-02-22 17:16:32 +0100

    Removed extra newline when converting to attributed string

--- Example/markymark/ViewController.swift
+++ Example/markymark/ViewController.swift
@@ -35,7 +35,6 @@ class ViewController: UIViewController {
             markDownView = getViewWithAttributedStringConverter(markDownItems)
         }
         
-        
         // Layout
         view.addSubview(markDownView)
 
