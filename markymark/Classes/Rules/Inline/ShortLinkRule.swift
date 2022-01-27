611c8eb2b74f4c05be19702645982bc056f220f6
Jim van Zummeren <jim@m2mobi.com>
2022-01-27 10:15:49 +0100

    Remove force unwrap in ShortLinkRule

--- markymark/Classes/Rules/Inline/ShortLinkRule.swift
+++ markymark/Classes/Rules/Inline/ShortLinkRule.swift
@@ -52,12 +52,17 @@ private extension ShortLinkRule {
     }
 
      func isValidURLString(_ string: String) -> Bool {
-        let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        if let match = detector.firstMatch(in: string, options: [], range: NSRange(location: 0, length: string.utf16.count)) {
-            // it is a link, if the match covers the whole string
-            return match.range.length == string.utf16.count
-        } else {
-            return false
-        }
+         do {
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+
+            if let match = detector.firstMatch(in: string, options: [], range: NSRange(location: 0, length: string.utf16.count)) {
+                // it is a link, if the match covers the whole string
+                return match.range.length == string.utf16.count
+            } else {
+                return false
+            }
+         } catch {
+             return false
+         }
     }
 }
